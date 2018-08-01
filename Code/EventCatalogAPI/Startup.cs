572022970cf016bbd7a0e52522ccd1c8ad9634e2
surabivishnupriya <35674260+surabivishnupriya@users.noreply.github.com>
2018-07-31 17:02:07 -0700

    added swagger

--- Code/EventCatalogAPI/Startup.cs
+++ Code/EventCatalogAPI/Startup.cs
@@ -29,7 +29,22 @@ namespace EventCatalogAPI
             services.AddMvc();
             services.AddDbContext<EventCatalogContext>(
                 options => options.UseSqlServer(Configuration["ConnectionString"]));
-        }
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1",
+                    new Swashbuckle.AspNetCore.Swagger.Info
+                    {
+                        Title = "EventMicroservices - Product Catalog HTTP API",
+                        Version = "v1",
+                        Description = "The Product Catalog Microservice HTTP API. This is a Data-Driven/CRUD microservice sample",
+                        TermsOfService = "Terms Of Service"
+                    });
+            });
+
+        
+    }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -38,6 +53,11 @@ namespace EventCatalogAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger()
+            .UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/v1/swagger.json", "EventCatalogAPI V1");
+            });
 
             app.UseMvc();
         }
