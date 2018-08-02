a11f059ca5220bb8076310d3dc2184a2f63cd95c
WenScarlet <8473652+wendyscarlet@users.noreply.github.com>
2018-08-01 17:58:00 -0700

    Added the Model to WishListAPI

---
+++ WishListAPI/Domain/WishCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishListAPI.Domain
+{
+    public class WishCart
+    {
+        List<WishCartItem> WishCartList;
+        public int BuyerId { get; set; }
+
+    }
+}
---
+++ WishListAPI/Domain/WishCartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishListAPI.Domain
+{
+    public class WishCartItem
+    {
+        public int Id { get; set; }
+        public int EventId { get; set; }
+        public string EventTitle { get; set; }
+        public decimal TicketPrice { get; set; }
+        public int NumOfTickets { get; set; }
+        public string TicketType { get; set; }
+    }
+}
