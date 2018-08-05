1b928eb03a20897201e5353e2bc507aae771c7f0
surabivishnupriya <35674260+surabivishnupriya@users.noreply.github.com>
2018-08-04 17:56:12 -0700

    Updated ticket controller

--- EventTicketAPI/Controllers/TicketController.cs
+++ EventTicketAPI/Controllers/TicketController.cs
@@ -49,7 +49,7 @@ namespace EventTicketAPI.Controllers
         }
         [HttpGet]
         [Route("Tickets/{id:int}")]
-        public async Task<IActionResult> GetEventById(int id)
+        public async Task<IActionResult> GetTicketById(int id)
         {
             if (id <= 0)
             {
@@ -61,12 +61,12 @@ namespace EventTicketAPI.Controllers
             {
                 return Ok(item);
             }
-                return NotFound();
+            return NotFound();
         }
-        //GET api/Events/withTitle/Burger Fest?pageSize=2&pageIndex=0
+        //GET api/Tickets/withTitle/Burger Fest?pageSize=2&pageIndex=0
         [HttpGet]
         [Route("[action]/withtitle/{title:minlength(1)}")]
-        public async Task<IActionResult> Tickets(string title,
+        public async Task<IActionResult> GetTicketsByEventTitle(string title,
             [FromQuery] int pageSize = 6,
             [FromQuery] int pageIndex = 0)
         {
@@ -85,10 +85,180 @@ namespace EventTicketAPI.Controllers
 
             return Ok(model);
         }
+        [HttpGet]
+        [Route("tickets/eventid/{eventId:int}")]
+
+        public async Task<IActionResult> GetTicketsbyEventId(int eventId,
+               [FromQuery] int pageSize = 6,
+               [FromQuery] int pageIndex = 0)
+        {
+            var totalItems = await _ticketCatalogContext.Tickets
+                               .Where(c => c.EventId == eventId)
+                              .LongCountAsync();
+
+            var itemsOnPage = await _ticketCatalogContext.Tickets
+                              .Where(c => c.EventId == eventId)
+                              .OrderBy(c => c.EventTitle)
+                              .Skip(pageSize * pageIndex)
+                              .Take(pageSize)
+                              .ToListAsync();
+
+
+            var model = new PaginatedEventTicketViewModel<Ticket>
+
+               (pageIndex, pageSize, totalItems, itemsOnPage);
+
+            return Ok(model);
+
+        }
+        [HttpGet]
+        [Route("tickets/tickettypeid/{typeId:int}")]
+
+        public async Task<IActionResult> GetTicketsByTicketTypeId(int typeId,
+               [FromQuery] int pageSize = 6,
+               [FromQuery] int pageIndex = 0)
+        {
+            var totalItems = await _ticketCatalogContext.Tickets
+                               .Where(c => c.TicketTypeId == typeId)
+                              .LongCountAsync();
+
+            var itemsOnPage = await _ticketCatalogContext.Tickets
+                              .Where(c => c.EventId == typeId)
+                              .OrderBy(c => c.EventTitle)
+                              .Skip(pageSize * pageIndex)
+                              .Take(pageSize)
+                              .ToListAsync();
+
+
+            var model = new PaginatedEventTicketViewModel<Ticket>
+
+               (pageIndex, pageSize, totalItems, itemsOnPage);
+
+            return Ok(model);
+
+        }
+
+        [HttpGet]
+        [Route("tickets/ByAvailableQuantity/{availableQuantity:int}")]
+
+        public async Task<IActionResult> GetTicketsByAvailableQuantity(int availableQuantity,
+              [FromQuery] int pageSize = 6,
+              [FromQuery] int pageIndex = 0)
+        {
+            var totalItems = await _ticketCatalogContext.Tickets
+                               .Where(c => c.AvailableQty >= availableQuantity)
+                              .LongCountAsync();
+
+            var itemsOnPage = await _ticketCatalogContext.Tickets
+                              .Where(c => c.AvailableQty >= availableQuantity)
+                              .OrderBy(c => c.EventTitle)
+                              .Skip(pageSize * pageIndex)
+                              .Take(pageSize)
+                              .ToListAsync();
+
+
+            var model = new PaginatedEventTicketViewModel<Ticket>
+
+               (pageIndex, pageSize, totalItems, itemsOnPage);
+
+            return Ok(model);
+
+        }
+
+        //commands
+
+        [HttpPost]
+
+        [Route("tickets")]
+        public async Task<IActionResult> CreateTicket([FromBody] Ticket newTicket)
+        {
+            var item = new Ticket
+
+            {
+                EventId = newTicket.EventId,
+                EventTitle = newTicket.EventTitle,
+                TicketPrice = newTicket.TicketPrice,
+                AvailableQty = newTicket.AvailableQty,
+                TicketTypeId = newTicket.TicketTypeId,
+                MinTktsPerOrder = newTicket.MinTktsPerOrder,
+                MaxTktsPerOrder = newTicket.MaxTktsPerOrder,
+                SalesStartDate = newTicket.SalesStartDate,
+                SalesEndDate = newTicket.SalesEndDate
+
+
+            };
+            _ticketCatalogContext.Tickets.Add(item);
+            await _ticketCatalogContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTicketById), new { id = item.Id });
+        }
+        [HttpPut]
+
+        [Route("tickets")]
+        public async Task<IActionResult> UpdateTicket([FromBody] Ticket ticketToUpdate)
+        {
+            var TicketItem = await _ticketCatalogContext.Tickets
+                              .SingleOrDefaultAsync
+                              (i => i.Id == ticketToUpdate.Id);
+            if (TicketItem == null)
+            {
+                return NotFound(new { Message = $"Item with id {ticketToUpdate.Id} not found." });
+            }
+
+
+            TicketItem.EventId = ticketToUpdate.EventId;
+            TicketItem.EventTitle = ticketToUpdate.EventTitle;
+            TicketItem.TicketPrice = ticketToUpdate.TicketPrice;
+            TicketItem.AvailableQty = ticketToUpdate.AvailableQty;
+            TicketItem.TicketTypeId = ticketToUpdate.TicketTypeId;
+            TicketItem.MinTktsPerOrder = ticketToUpdate.MinTktsPerOrder;
+            TicketItem.MaxTktsPerOrder = ticketToUpdate.MaxTktsPerOrder;
+            TicketItem.SalesStartDate = ticketToUpdate.SalesStartDate;
+            TicketItem.SalesEndDate = ticketToUpdate.SalesEndDate;
 
 
 
+            _ticketCatalogContext.Tickets.Add(TicketItem);
+            await _ticketCatalogContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTicketById), new { id = ticketToUpdate.Id });
+        }
 
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteTicket(int id)
+        {
+            var ticketItem = await _ticketCatalogContext.Tickets
+                .SingleOrDefaultAsync(p => p.Id == id);
+            if (ticketItem == null)
+            {
+                return NotFound();
+
+            }
+            _ticketCatalogContext.Tickets.Remove(ticketItem);
+            await _ticketCatalogContext.SaveChangesAsync();
+            return NoContent();
+
+        }
+        [HttpDelete]
+        [Route("deletealltickets/{eventid}")]
+        public async Task<IActionResult> DeleteTicketsByEventId(int eventid)
+        {
+            var ticketItem = await _ticketCatalogContext.Tickets
+                .Where(p => p.EventId == eventid).ToListAsync();
+            if (ticketItem == null)
+            {
+                return NotFound(new { Message = $"Tickets with eventid {eventid} not found." });
+
+            }
+            _ticketCatalogContext.Tickets.RemoveRange(ticketItem);
+            await _ticketCatalogContext.SaveChangesAsync();
+            return NoContent();
+
+        }
 
     }
 }
+
+
+
+
