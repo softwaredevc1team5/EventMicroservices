6378ed4e7ca7b4b12f743d466baa8d1f37031daf
Lisa Hare <hare.lisa@gmail.com>
2018-08-12 12:56:46 -0700

    adding EventDetail models

---
+++ Code/WebMvc/ViewModels/EventDetailViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.ViewModels
+{
+    public class EventDetailViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string ImageUrl { get; set; }
+        public Decimal Price { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int EventTypeId { get; set; }
+        public int EventCategoryId { get; set; }
+        public string EventType { get; set; }
+        public string EventCategory { get; set; }
+        public int OrganizerId { get; internal set; }
+        public string OrganizerName { get; set; }
+    }
+}
