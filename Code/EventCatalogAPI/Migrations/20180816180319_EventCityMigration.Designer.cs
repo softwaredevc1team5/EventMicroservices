9d08c8e67befa31015ecae8f139c8c0f51bac320
bhuvaneswarim <bhuvaneswari.m10@gmail.com>
2018-08-16 13:15:52 -0700

    commit

--- Code/EventCatalogAPI/Migrations/20180816180319_EventCityMigration.Designer.cs
+++ Code/EventCatalogAPI/Migrations/20180816180319_EventCityMigration.Designer.cs
@@ -4,14 +4,16 @@ using EventCatalogAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EventCatalogAPI.Migrations
 {
     [DbContext(typeof(EventCatalogContext))]
-    partial class EventCatalogContextModelSnapshot : ModelSnapshot
+    [Migration("20180816180319_EventCityMigration")]
+    partial class EventCityMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
