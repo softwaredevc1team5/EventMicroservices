ae9d12f342e7d74cf5d6231e3983137649d8569a
chitraphp <chitra.atmakuru@gmail.com>
2018-08-23 12:52:03 -0700

    Add changes

--- Code/EventCatalogAPI/Controllers/EventController.cs
+++ Code/EventCatalogAPI/Controllers/EventController.cs
@@ -465,8 +465,15 @@ namespace EventCatalogAPI.Controllers
                 root = root.Where(c => c.City.ToLower() == eventCity.ToLower() );
                 //root = root.Where(c => c.City == citystr && c.State == statestr);
             }
+           /*** if(eventTitle != "null")
+            {
+                StringComparison comp = StringComparison.OrdinalIgnoreCase;
 
+                root = root.Where(c => c.Title.Contains(eventTitle, comp) == true)
+            }
+            ***/
             if (eventDate != "null" && eventDate != "All Days")
+
             {
                 root = FindingEventsByDate(root, eventDate);
 
