269efed3c324718fee8e7c847ef309f3049e3913
WenScarlet <8473652+wendyscarlet@users.noreply.github.com>
2018-08-24 09:48:06 -0700

    Add the model for Order

---
+++ Code/WebMvc/Models/Order/Order.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Models.Orders
+{
+    public class Order
+    {     
+        [BindNever]
+        public int OrderId { get; set; }
+
+        [BindNever]
+        public DateTime OrderDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        public decimal OrderTotal { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [BindNever]
+        public string UserName { get; set; }
+
+        [BindNever]
+
+        public string BuyerId { get; set; }
+
+        public string StripeToken { get; set; }
+
+        public OrderStatus OrderStatus { get; set; }
+        
+        //EventProperties
+        public int EventId { get; set; }
+
+        public string EventTitle { get; set; }
+
+        public DateTime EventStartDate { get; set; }
+
+        public DateTime EventEndDate { get; set; }
+
+        public string PictureUrl { get; set; }
+
+
+    }
+
+    public enum OrderStatus
+    {
+        Preparing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Code/WebMvc/Models/Order/OrderTicketCatalog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Models.Orders
+{
+    public class OrderTicketCatalog
+    {
+
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+
+        public int Count { get; set; }
+
+        public List<OrderTicket> Data { get; set; }
+    }
+}
+
