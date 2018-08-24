0c02b64f11d4bf6c4585c9e181accbba18bfe34a
WenScarlet <8473652+wendyscarlet@users.noreply.github.com>
2018-08-24 09:48:18 -0700

    Add the services for Order

---
+++ Code/WebMvc/Services/Order/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc.Models.Orders;
+
+namespace WebMvc.Services.Orders
+{
+    interface IOrderService
+    {
+        Task<Order> GetOrdersByBuyer(int buyerId, int page, int take);
+        Task<Order> GetOrderById(int orderId);
+    }
+}
---
+++ Code/WebMvc/Services/Order/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc.Models.Orders;
+
+namespace WebMvc.Services.Orders
+{
+    public class OrderService : IOrderService
+    {
+        public Task<Order> GetOrderById(int orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrdersByBuyer(int buyerId, int page, int take)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
