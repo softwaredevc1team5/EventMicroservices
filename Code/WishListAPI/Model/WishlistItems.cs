189cccb5e9f330608084601f8f9b07eee353313c
surabivishnupriya <35674260+surabivishnupriya@users.noreply.github.com>
2018-08-28 18:36:06 -0700

    Display ticket price

--- Code/WishListAPI/Model/WishlistItems.cs
+++ Code/WishListAPI/Model/WishlistItems.cs
@@ -10,7 +10,7 @@ namespace WishListAPI.Model
         public string Id { get; set; }
         public string productId { get; set; }
         public string EventTitle { get; set; }
-        public decimal Price { get; set; }
+        public decimal TicketPrice { get; set; }
         public decimal OldTicketPrice { get; set; }
         public string  City{ get; set; }
         public string TicketType { get; set; }
